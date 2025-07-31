d31bdfd27e73b6de4e452ffec08a5370746750a2
duoladeAmeng <1107128204@qq.com>
2025-07-31 11:12:43 +0800

    引入Tailwind CSS

--- src/pages/admin/Login.vue
+++ src/pages/admin/Login.vue
@@ -1,36 +1,9 @@
 <script setup>
 </script>
 <template>
-    <div class="p-2">
-        <h1>登录页</h1>
-        <el-row class="mb-4">
-            <el-button>Default</el-button>
-            <el-button type="primary">Primary</el-button>
-            <el-button type="success">Success</el-button>
-            <el-button type="info">Info</el-button>
-            <el-button type="warning">Warning</el-button>
-            <el-button type="danger">Danger</el-button>
-        </el-row>
-
-        <el-row class="mb-4">
-            <el-button plain>Plain</el-button>
-            <el-button type="primary" plain>Primary</el-button>
-            <el-button type="success" plain>Success</el-button>
-            <el-button type="info" plain>Info</el-button>
-            <el-button type="warning" plain>Warning</el-button>
-            <el-button type="danger" plain>Danger</el-button>
-        </el-row>
-
-        <el-row class="mb-4">
-            <el-button round>Round</el-button>
-            <el-button type="primary" round>Primary</el-button>
-            <el-button type="success" round>Success</el-button>
-            <el-button type="info" round>Info</el-button>
-            <el-button type="warning" round>Warning</el-button>
-            <el-button type="danger" round>Danger</el-button>
-        </el-row>
-
-    </div>
+      <div class="bg-green-300 inline">绿色</div>
+    <div class="bg-yellow-300 ml-2 inline">黄色</div>
+    <div class="bg-blue-300 ml-2 inline">蓝色</div>
 </template>
 
 <style scoped>
